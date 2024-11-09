6abd41bebb61d605b04d87216ed2b8fb95cdf284
constancefr <constance.frachon@gmail.com>
2024-11-09 12:04:46 +0000

    clean up

--- sphere.h
+++ sphere.h
@@ -1,18 +1,18 @@
-#ifndef SPHERE_H
-#define SPHERE_H
+// #ifndef SPHERE_H
+// #define SPHERE_H
 
-#include <cmath>
-#include "vector3.h"
+// #include <cmath>
+// #include "vector3.h"
 
-class Sphere {
-public:
-    Sphere(Vector3 center, float radius);
+// class Sphere {
+// public:
+//     Sphere(Vector3 center, float radius);
 
-    bool intersect(const Vector3& rayOrigin, const Vector3& rayDir, float& t1, float& t2) const;
+//     bool intersect(const Vector3& rayOrigin, const Vector3& rayDir, float& t1, float& t2) const;
 
-private:
-    Vector3 center;
-    float radius;
-};
+// private:
+//     Vector3 center;
+//     float radius;
+// };
 
-#endif
+// #endif
